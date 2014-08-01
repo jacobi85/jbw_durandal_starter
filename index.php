a67a5491d5bef6487d1fa7cb40421dc942fa0465
Jacob Westman <jacob.devillusion@gmail.com>
2014-08-01 10:31:37 +0200

    Added basic index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>In motion for change/title>
+        <title>In motion for change</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width">
         <!-- ><link rel="stylesheet/less" type="text/css" href="css/style.less" /> -->
