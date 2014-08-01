a90dc588a92692659658f34bb58dd930391ad767
Jacob Westman <jacob.devillusion@gmail.com>
2014-08-01 18:11:41 +0200

    Installed Bootstrap Template

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>In motion for change</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width">
-        <!-- ><link rel="stylesheet/less" type="text/css" href="css/style.less" /> -->
-    </head>
-    <body>
-        <h1>Welcome to the Matrix</h1>
-    </body>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/mystyles.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscript.js"></script>
+  </body>
 </html>
