e1f0dd30ae29fa725c011aa82489b3144ba03811
Jacob Westman <jacob.devillusion@gmail.com>
2014-08-01 10:28:58 +0200

    Added basic index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>In motion for change/title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <!-- ><link rel="stylesheet/less" type="text/css" href="css/style.less" /> -->
+    </head>
+    <body>
+        <h1>Test</h1>
+    </body>
+</html>
