01e2654b68c9001e506beb993538f8d7c3675d7b
Jacob Westman <jacob.devillusion@gmail.com>
2014-08-20 12:28:51 +0200

    updated bootstrap template

---
+++ app/shell.js
@@ -0,0 +1,13 @@
+﻿define(['plugins/router'], function(router) {
+    return {
+        router: router,
+        activate: function() {
+            return router.map([
+                {route: ['', 'home'], moduleId: 'viewmodels/home', title: 'Hello World', nav: 1},
+                {route: 'example-page', moduleId: 'viewmodels/page', title: 'Example page', nav: true}
+            ]).buildNavigationModel()
+                    .mapUnknownRoutes('viewmodels/home', 'not-found')
+                    .activate();
+        }
+    };
+});
